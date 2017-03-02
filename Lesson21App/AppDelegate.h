f070620b64e8f98e7ece1aa709147b7c9792ed65
Александр Горбачев <gorby@MacBook-Pro-AG.lan>
2017-03-02 15:43:03 +0300

    Lesson21App: My first commit

---
+++ Lesson21App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson21App
+//
+//  Created by Александр Горбачев on 02.03.17.
+//  Copyright © 2017 gorby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson21App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson21App
+//
+//  Created by Александр Горбачев on 02.03.17.
+//  Copyright © 2017 gorby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
